0901b9cd73393b1c7d211ddeda8abacaeffc76ef
Justin <invicitusreshat@outlook.com>
2023-12-11 11:15:33 -0600

    adding project folder

--- Scrumptious project folder/urls.py
+++ Scrumptious project folder/urls.py
@@ -18,12 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 
-def redirect_to_recipe_list(request):
-    return redirect("recipe_list")
 
 urlpatterns = [
     path("", redirect_to_recipe_list, name='home_page'),
     path('admin/', admin.site.urls),
     path("recipes/", include("recipes.urls")),
-    path("accounts/", include("accounts.urls")),
 ]
